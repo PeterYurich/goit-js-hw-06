81628a17f24f7619e66c15ea335290d73266a3fa
Peter <peteryurich@gmail.com>
2022-07-07 00:55:13 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,33 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// function makeIngridientEl (ingredient) {
+//   const ingredientEl = document.createElement("li")
+//   ingredientEl.textContent = `${ingredient}`
+//   ingredientEl.classList.add("item")
+//   ingredientsEls.push(ingredientEl)
+// }
+
+const ingredientsEls = []
+ingredients.forEach(function (ingredient)  {
+  const ingredientEl = document.createElement("li")
+  ingredientEl.textContent = `${ingredient}`
+  ingredientEl.classList.add("item")
+  ingredientsEls.push(ingredientEl)
+})
+// лучше будет так написать функцию для forEach или лучше было 
+// бы вынести её в makeIngridientEl и передать колбеком в forEach?
+
+document.querySelector("#ingredients").append(...ingredientsEls)
+
+
+// experiment: (почему через map получается коллекция из шести undefind ?)
+
+// const ingredientsEls = ingredients.map((ingredient) => {
+//   const ingredientEl = document.createElement("li")
+//   ingredientEl.textContent = `${ingredient}`
+//   ingredientEl.classList.add("item")
+// })
+
+// document.querySelector("#ingredients").append(...ingredientsEls)
