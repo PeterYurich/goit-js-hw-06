c96c2367a156a9c76078c6b23c6de9cb405dcdc6
Peter <peteryurich@gmail.com>
2022-07-10 23:57:20 +0300

    com

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+const emailInputEl = document.querySelector("[name=email]");
+const passwordInputEl = document.querySelector("[name=password]");
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (emailInputEl.value === "" || passwordInputEl.value === "") {
+        alert("Every input has to be filled in")
+    }
+    const {
+        
+    }
+})
+
+// 
+// console.log(submitBtnEl)
