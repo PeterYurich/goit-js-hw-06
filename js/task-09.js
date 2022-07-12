8b209957eecf24daeff5eef1a0d91e77b46d1cc9
Peter <peteryurich@gmail.com>
2022-07-12 13:57:14 +0300

    task-10-doing

--- js/task-09.js
+++ js/task-09.js
@@ -17,4 +17,3 @@ const changeColorHandler = () => {
 }
 
 changeColorBtnEl.addEventListener('click', changeColorHandler);
-   
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxContainerEl = document.querySelector("#boxes");
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+
+// const boxAmount = document.querySelector("[type=number").value;
+
+// console.log(boxAmount)
+
+
+const createBoxes = () => {
+  console.log('the function is done')
+
+  const amount = document.querySelector("[type=number").value;
+
+
+  const boxesArray = [];
+
+  for (let i = 0; i <= amount; i += 1) {
+    let box = document.createElement("div");
+    boxesArray.push("<div></div>");
+  }
+
+  boxContainerEl.insertAdjacentHTML("afterbegin", boxesArray.join(""));
+
+  console.log(boxContainerEl.children);
+
+}
+
+createBtnEl.addEventListener('click', createBoxes)
+
+
+
+
